c76c5f592c0ca6f0faf021dd49d7942d42001f81
Ridzen <ridze00@gmail.com>
2022-11-29 20:01:03 +0600

    most part done

--- main.py
+++ main.py
@@ -10,16 +10,18 @@ import requests
 # class PostRestMethod(): //можно через класс(если будем расширять(хотя это противоречит принципу Open/Close),
 # но лучше конечно же через обычные переменные)
 
-response = requests.post('https://httpbin.org/post', json={'name': 'test', 'time': '18:37 26.11.2022'})
+response = requests.post('https://httpbin.org/post', json={'name': 'test', 'time': '11:25 26.11.2022'})
+# не нашел информацию по TLCListener, но и этот запрос рабочий типо
 json_response = response.json()
 datetime = datetime.now()
+print(response)
+# Ответ приходит, <Response [200]> (круто верно?)
 
-
-if datetime == response.request.body:
+if datetime == response.request.body:  # Но на этой строке он почемуто ломается и не выводит текущую дату, суть в том что он должен отработать когда наступит время(time), но сейчас почему-то не работает
     print('Выполнение команды')
     if response.status_code == 200:
         print('Success!')
     elif response.status_code == 404:
         print('Not Found.')
 else:
-    print("иди в жопу")
+    print("Ничего не работает")
